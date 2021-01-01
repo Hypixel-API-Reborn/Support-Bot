c9ea6ad02f155f3ad3dcbfa4c54ef12dd88e54b8
StavZDev <randy10220@gmail.com>
2021-01-02 00:49:38 +0500

    Update wrapper.js

--- src/commands/Hypixel/wrapper.js
+++ src/commands/Hypixel/wrapper.js
@@ -37,15 +37,16 @@ class WrapperCommand extends Command {
   async exec (message, args) {
     if (!args.method) return message.channel.send('I need to know which method to call.');
     args.args = args.args.split(/ +/);
-    if (args.args.length > 1 && args.method !== 'getGuild') return message.channel.send('Too many arguments. Only 1 is allowed, except for getGuild');
-    const result = await hypixel[args.method](...args);
+    if (args.args.length > 2 && args.method !== 'getGuild') return message.channel.send('Too many arguments. Only 1 is allowed, except for getGuild');
+    args.args.shift();
+    const result = await hypixel[args.method](...args.args);
     if (args.noParse) {
       return message.channel.send('API Result : ', { files: [JSON.stringify(result)] });
     }
     return message.channel.send(new MessageEmbed()
       .addFields(this.format(result))
       .addField('\u200B', 'More results might be hidden')
-      .setColor('RANDOM')
+      .setColor(this.client.color)
       .setTitle(args.method)
       .setTimestamp()
     );
@@ -59,11 +60,11 @@ class WrapperCommand extends Command {
     if (Array.isArray(result)) return this.format(result[0]);
     if (typeof result === 'object') {
       const obj = Object.getOwnPropertyNames(result).reduce((a, b) => {
-        if (Array.isArray(result[b])) a[b] = result[b].toString().join(',');
+        if (Array.isArray(result[b])) a[b] = result[b].join(', ');
         else a[b] = typeof result[b] === 'object' ? JSON.stringify(result[b]) : result[b];
         return a;
       }, {});
-      return Object.keys(obj).map(x => ({ name: x, value: obj[x].toString() })).slice(0, 24);
+      return Object.keys(obj).map(x => ({ name: x, value: obj[x].toString().length > 1024 ? obj[x].toString().slice(0, 1024) : obj[x].toString() })).slice(0, 24);
     }
     return [{ name: 'Response', value: String(result) }];
   }
