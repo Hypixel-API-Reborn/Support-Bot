09f43c294c6cde1335845bb61a2834ea07e04f1b
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2021-01-01 15:58:42 +0100

    Update wrapper.js
    correct lint

--- src/commands/Hypixel/wrapper.js
+++ src/commands/Hypixel/wrapper.js
@@ -1,3 +1,4 @@
+const { Command } = require('discord-akairo');
 const { Message, MessageEmbed } = require('discord.js');
 const hypixel = require('../../Hypixel');
 const { methodAlias } = require('../../constants.json');
@@ -36,9 +37,9 @@ class WrapperCommand extends Command {
   async exec (message, args) {
     if (!args.method) return message.channel.send('I need to know which method to call.');
     args.args = args.args.split(/ +/);
-    if (args.args.length > 1 && method !== 'getGuild') return message.channel.send('Too many arguments. Only 1 is allowed, except for getGuild');
+    if (args.args.length > 1 && args.method !== 'getGuild') return message.channel.send('Too many arguments. Only 1 is allowed, except for getGuild');
     const result = await hypixel[args.method](...args);
-    if (noParse) {
+    if (args.noParse) {
       return message.channel.send('API Result : ', { files: [JSON.stringify(result)] });
     }
     return message.channel.send(new MessageEmbed()
@@ -59,12 +60,12 @@ class WrapperCommand extends Command {
     if (typeof result === 'object') {
       const obj = Object.getOwnPropertyNames(result).reduce((a, b) => {
         if (Array.isArray(result[b])) a[b] = result[b].toString().join(',');
-        else a[b] = typeof result[b] === 'object' ? JSON.stringify(O(result[b])) : result[b];
+        else a[b] = typeof result[b] === 'object' ? JSON.stringify(result[b]) : result[b];
         return a;
       }, {});
       return Object.keys(obj).map(x => ({ name: x, value: obj[x].toString() })).slice(0, 24);
     }
-    return [{ name: 'Response', value: String(object) }];
+    return [{ name: 'Response', value: String(result) }];
   }
 }
 module.exports = WrapperCommand;
