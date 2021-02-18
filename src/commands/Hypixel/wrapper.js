42b737a3e364c5c68d0f665f0213637895638fc7
StavZDev <randy10220@gmail.com>
2021-02-18 17:31:01 +0300

    Update wrapper.js

--- src/commands/Hypixel/wrapper.js
+++ src/commands/Hypixel/wrapper.js
@@ -51,7 +51,7 @@ class WrapperCommand extends Command {
       return message.channel.send(embed);
     }
     args.args = args.args.split(/ +/);
-    const gte = (args.args.indexOf('>') + 1 || args.args.length) - 1;
+    const gte = (args.args.indexOf('>') + 1 || args.args.length + 1) - 1;
     const query = args.args.slice(0, gte);
     const path = args.args.slice(gte).join('').slice(1).split('>').filter((x) => x !== '');
     if (query.length > 2 && args.method !== 'getGuild') return message.channel.send('Too many arguments. Only 1 is allowed, except for getGuild');
@@ -63,7 +63,8 @@ class WrapperCommand extends Command {
     path.forEach((element) => result = result ? result[element] : undefined);
     if (result === undefined) return message.channel.send('API Result is undefined. Maybe it is due to an invalid path or simply because the result is undefined.');
     if (args.noParse && result) {
-      const attachment = new MessageAttachment(Buffer.from(JSON.stringify(result, null, 4)), 'result.json');
+      const date = require('moment')().format('MM-DD_HH-mm');
+      const attachment = new MessageAttachment(Buffer.from(JSON.stringify(result, null, 4)), `result_${date}.json`);
       return message.channel.send('API Result : ', { files: [attachment] });
     }
     return message.channel.send(new MessageEmbed()
